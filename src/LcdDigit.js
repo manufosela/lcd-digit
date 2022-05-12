40e82fe6ca9e237dd35b353a2909a7ec04b64745
Manuel Juan Fosela Águila <manufosela@gmail.com>
2022-05-12 18:13:09 +0200

    chore:  refactor styles into an import file

--- src/LcdDigit.js
+++ src/LcdDigit.js
@@ -1,48 +1,10 @@
-import {
-  html,
-  css,
-  LitElement
-} from 'lit';
-
+import { html, LitElement } from 'lit';
+import { lcdDigitStyles } from './lcd-digit-styles.js'
 import { numbersLcdMatrix } from './numbersLcdMatrix.js';
 
 export class LcdDigit extends LitElement {
   static get styles() {
-    return css /* css */`
-      :host {
-        width: auto;
-        display: block;
-        background-color: transparent;
-        --dot-size: 16px;
-        --dot-margin: calc(var(--dot-size) / 8);
-      }
-      .digit {
-        width: calc(var(--dot-size) * 4 + calc(var(--dot-margin) * 8));
-        display: inline-block;
-        margin: 10px;
-        transform: skewX(-2deg);
-        transition: opacity 100ms ease;
-      }
-      .digit .cell {
-        width: var(--dot-size);
-        height: var(--dot-size);
-        margin: var(--dot-margin);
-        background-color: red;
-        border-radius: 2px;
-        display: inline-block;
-        opacity: 0.1;
-      }
-      .digit .cell.on {
-        opacity: 1;
-      }
-      .dot { 
-        position: relative; 
-        background: #ff0; 
-        z-index: 10;
-        width: var(--dot-size); 
-        height: var(--dot-size);  
-      }
-    `;
+    return [lcdDigitStyles];
   }
 
   static get properties() {
