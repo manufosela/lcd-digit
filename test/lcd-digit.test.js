af6d905af7727bdbb2a31b8d6270463b2e5aeb81
Manuel Juan Fosela Águila <manufosela@gmail.com>
2022-05-19 20:42:53 +0200

    feat: add attributes test and shadow-dom

--- test/lcd-digit.test.js
+++ test/lcd-digit.test.js
@@ -2,41 +2,70 @@ import { html, fixture, expect } from '@open-wc/testing';
 
 import '../lcd-digit.js';
 
+/**
+ *  this.digit = 'empty';
+    this.count = false;
+    this.lcdReference = '';
+    this.increment = 1;
+    this.maxValue = 9;
+    this.intervalId = null;
+ */
 describe('LcdDigit', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture(html`
-      <lcd-digit></lcd-digit>
-    `);
+  it('has a default lcdReference is "", count is false, digit is "empty", increment is 1 and maxValue is 9', async () => {
+    const el = await fixture(html` <lcd-digit></lcd-digit> `);
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    expect(el.digit).to.equal('empty');
+    expect(el.count).to.equal(false);
+    expect(el.lcdReference).to.equal('');
+    expect(el.increment).to.equal(1);
+    expect(el.maxValue).to.equal(9);
   });
 
-  it('shows initially the text "hey there Nr. 5!" and an "increment" button', async () => {
-    const el = await fixture(html`
-      <lcd-digit></lcd-digit>
-    `);
+  it('shows initially the digit 8 with 4*7 divs', async () => {
+    const el = await fixture(html` <lcd-digit></lcd-digit> `);
 
     expect(el).shadowDom.to.equal(`
-      <h2>Hey there Nr. 5!</h2>
-      <button>increment</button>
+      <div id="digit" class="digit">
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div>
+      </div>
     `);
   });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture(html`
-      <lcd-digit></lcd-digit>
-    `);
-    el.shadowRoot.querySelector('button').click();
+  it('can override the digit via attribute', async () => {
+    const el = await fixture(html` <lcd-digit digit="8"></lcd-digit> `);
 
-    expect(el.counter).to.equal(6);
+    expect(el.digit).to.equal(8);
   });
 
-  it('can override the title via attribute', async () => {
+  it('can override the increment via attribute', async () => {
+    const el = await fixture(html` <lcd-digit increment="-1"></lcd-digit> `);
+
+    expect(el.increment).to.equal(-1);
+  });
+
+  it('can override the maxValue via attribute', async () => {
+    const el = await fixture(html` <lcd-digit max-value="5"></lcd-digit> `);
+
+    expect(el.maxValue).to.equal(5);
+  });
+
+  it('can override the count via attribute', async () => {
+    const el = await fixture(html` <lcd-digit digit="1" count></lcd-digit> `);
+
+    expect(el.count).to.equal(true);
+  });
+
+  it('can override the lcdReference via attribute', async () => {
     const el = await fixture(html`
-      <lcd-digit title="attribute title"></lcd-digit>
+      <lcd-digit lcd-reference="secUnits"></lcd-digit>
     `);
 
-    expect(el.title).to.equal('attribute title');
+    expect(el.lcdReference).to.equal('secUnits');
   });
 });
